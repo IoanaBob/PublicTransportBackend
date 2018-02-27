10b9fceb78c8dba5b84c5f2dcbfb96ce49778b3d
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-02-27 15:07:17 +0000

    spec on location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,14 @@
 class Location < ApplicationRecord
+  validates :latitude, presence: true, numericality: {only_float: true}
+  validates :longitude, presence: true, numericality: {only_float: true}
+  validates :current_speed, presence: true, numericality: {only_float: true}
+  validates :time, presence: true
+  validate :location_in_the_past
   belongs_to :bus_stop
+
+  def location_in_the_past
+    if time.present? && time > DateTime.now
+      errors.add(:time, "cannot be in the future")
+    end
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    context "when valid" do
+      subject { FactoryBot.create :location }
+      it { expect { subject }.to change { Location.count }.by(1) }
+    end
+
+    context "when invalid" do
+      subject { FactoryBot.create :location, latitude: "not a coordinate" }
+      
+      it do
+        expect { subject }
+          .to raise_error(ActiveRecord::RecordInvalid)
+          .and change { Location.count }.by(0)
+      end
+    end
+
+    describe "#latitude" do
+      subject { FactoryBot.build :location, latitude: latitude }
+
+      context "when not float" do
+        let(:latitude) { "hi" }
+        it { is_expected.not_to be_valid }
+      end
+    end
+    
+    describe "#longitude" do
+      subject { FactoryBot.build :location, longitude: longitude }
+
+      context "when not float" do
+        let(:longitude) { "hi" }
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe "#current_speed" do
+      subject { FactoryBot.build :location, current_speed: current_speed }
+
+      context "when not float" do
+        let(:current_speed) { "hi" }
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe "#time" do
+      subject { FactoryBot.build :location, time: time }
+
+      context "when not datetime" do
+        let(:time) { nil }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "when later than current time" do
+        let(:time) { "2999-02-27 14:25:17" }
+        it { is_expected.not_to be_valid }
+      end
+    end
+  end
 end
