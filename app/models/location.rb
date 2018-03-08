29dea2d28f47ad580b19582a7ae01f5a75e2c970
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-03-08 22:04:51 +0000

    new scopes in location for clear querying

--- app/models/location.rb
+++ app/models/location.rb
@@ -20,6 +20,20 @@ class Location < ApplicationRecord
     return (1..5).include? day
   end
 
+  scope :where_aimed_time, -> (time) { where("to_char(aimed_departure_time, 'HH24:MI') = '#{time}'") }
+  scope :where_aimed_in_time_range, -> (starting, ending) { where("aimed_departure_time::time between '#{starting}'::time and '#{ending}'::time") }
+
+  scope :where_weekday, lambda { |valid|
+    if valid == true
+      where("EXTRACT(DOW FROM \"aimed_departure_time\") = ANY('{1,2,3,4,5}'::int[])")
+    else
+      where("EXTRACT(DOW FROM \"aimed_departure_time\") = ANY('{0,6}'::int[])")
+    end
+  }
+
+  scope :average_delay, -> { average(:delay) }
+  # scope :where_weekend, -> (date, valid) {valid == true ? where("EXTRACT(DOW FROM TIMESTAMP '2001-02-16 20:38:40') == ANY('{1,2,3,4,5}'::int[])") : where("EXTRACT(DOW FROM TIMESTAMP '2001-02-16 20:38:40') == ANY('{0,6}'::int[])") }
+
   def expected_time
     (time.to_time - delay.minutes).to_datetime
   end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -78,15 +78,93 @@ RSpec.describe Location, type: :model do
       let!(:time) { "2018-02-24 14:25:17" }
       subject { (FactoryBot.build :location, delay: delay, time: time).expected_time }
 
-      describe "when delay positive" do
+      context "when delay positive" do
         let(:delay) { 3 }
         it { is_expected.to eq("2018-02-24 14:22:17")}
       end
 
-      describe "when delay negative" do
+      context "when delay negative" do
         let(:delay) { -5 }
         it { is_expected.to eq("2018-02-24 14:30:17")}
       end
+    end
+  end
+
+  describe "scope" do
+    describe "where_aimed_time" do
+      before { FactoryBot.create :location }
+      subject { Location.where_aimed_time(time).first }
+      
+      context "when time = aimed time" do
+        let!(:time) { "14:30" }
+        it {is_expected.to have_attributes(Location.first.attributes)}
+      end
+      context "when time != aimed time" do
+        let!(:time) { "14:20" }
+        it { is_expected.to be_blank }
+      end
+    end
+
+    describe "aimed_in_time_range" do
+      before { FactoryBot.create :location }
+      subject { Location.where_aimed_in_time_range(starting, ending).first }
+
+      context "when location in time range" do
+        let(:starting) { "14:00" }
+        let(:ending) { "16:00" }
+        it {is_expected.to have_attributes(Location.first.attributes)}
+      end
+
+      context "when location not in time range" do
+        let(:starting) { "12:30" }
+        let(:ending) { "13:30" }
+        it { is_expected.to be_blank }
+      end
+    end
+
+    describe "where_weekday" do
+      context "when choosing weekdays" do
+        before { FactoryBot.create :location, aimed_departure_time: "2018-02-23 14:25:17" }
+        subject { Location.where_weekday(valid).first }
+
+        context "when valid = true" do
+          let(:valid) { true }
+          it {is_expected.to have_attributes(Location.first.attributes)}
+        end
+
+        context "when valid = false" do
+          let(:valid) { false }
+          it { is_expected.to be_blank }
+        end
+      end
+
+      context "when choosing weekends" do
+        let!(:location) { FactoryBot.create :location, aimed_departure_time: "2018-02-24 14:25:17" }
+        subject { Location.where_weekday(valid).first }
+
+        context "when valid = true" do
+          let(:valid) { true }
+          it { is_expected.to be_blank }
+        end
+        context "when valid = false" do
+          let(:valid) { false }
+          it {is_expected.to have_attributes(Location.first.attributes)}
+        end
+      end
+    end
+
+    describe "avergae_delay" do
+      subject { Location.average_delay }
+
+      context "when empty" do
+        it { is_expected.to be_blank }
+      end
+
+      context "when two elements" do
+        before { FactoryBot.create :location, delay: 2.0; FactoryBot.create :location, delay: 5.0 }
+        
+        it { is_expected.to eq(3.5) }
+      end    
     end
   end
 end
